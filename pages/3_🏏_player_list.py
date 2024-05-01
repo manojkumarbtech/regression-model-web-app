8447fc2d386dd314e6191067cbc1f347d8812e22
Aniket <aniket.modi117@protonmail.com>
2024-05-01 10:23:44 +0530

    plyr list pg improvement

--- pages/3_🏏_player_list.py
+++ pages/3_🏏_player_list.py
@@ -9,15 +9,15 @@ filepath_player_auc = 'csv files/IPL_2023-22_Sold_Players.csv'
 player_def = "Virat Kohli"
 
 
-
 with st.sidebar:
     player = st.text_input(label=prompt_1, value=player_def)
 
     stats_op = st.selectbox(f"Select the stats you want to see of {player}",
-                        ('Batting Stats', 'Bowling Stats'), key='stats_op')
+                            ('Batting Stats', 'Bowling Stats'), key='stats_op')
 
 # define select-box items
 col_pl = []
+pl_col = []
 
 # define filepath for player stats
 filepath_cric = ""
@@ -43,16 +43,22 @@ if player:
 
     if 'Batting Stats' == stats_op:
         # display batting stats of the player
-        col_pl = ["POS", "Mat", "Inns", "NO", "Runs",
-                  "HS", "Avg", "BF", "SR", "100", "50", "4s", "6s", "Player"
+        col_pl = ["POS", "Player", "Mat", "Inns", "NO", "Runs",
+                  "HS", "Avg", "BF", "SR", "100", "50", "4s", "6s"
+                  ]
+        pl_col = ["POS", "Player", "Mat", "Inns", "NO", "Runs",
+                  "HS", "Avg", "BF", "SR", "100", "50", "4s", "6s"
                   ]
 
         filepath_cric = "csv files/BATTING STATS - IPL_2022.csv"
 
     if 'Bowling Stats' == stats_op:
         # display bowling stats of the player
-        col_pl = ["POS", "Mat", "Inns", "Ov", "Runs", "Wkts",
-                  "BBI", "Avg", "Econ", "SR", "4w", "5w", "PLayer"
+        col_pl = ["POS", "Player", "Mat", "Inns", "Ov", "Runs", "Wkts",
+                  "BBI", "Avg", "Econ", "SR", "4w", "5w"
+                  ]
+        pl_col = ["POS", "Player", "Mat", "Inns", "Ov", "Runs", "Wkts",
+                  "BBI", "Avg", "Econ", "SR", "4w", "5w"
                   ]
 
         filepath_cric = "csv files/BOWLING STATS - IPL_2022.csv"
@@ -70,13 +76,19 @@ except FileNotFoundError:
 
 try:
     with st.sidebar:
-        option = st.selectbox("Select data to view for the specific player", col_pl,
-                              key='player_op')
+
+        # index argument changes the values which col_pl is referring to
+        option = st.selectbox("Select data to view for the specific player",
+                              col_pl,
+                              key='player_op',
+                              index=2)
 
         col_pl.remove(option)
 
-        option_2 = st.selectbox("Select data to view for the specific player", col_pl,
-                                key='player_op_2')
+        option_2 = st.selectbox("Select data to view for the specific player",
+                                col_pl,
+                                key='player_op_2',
+                                index=6)
 
     player_value = data.loc[data['Player'] == player, (option, option_2)].values[0]
     st.info(f"{player} is a {player_role} whose {option} is/are {player_value[0]} " +
@@ -87,7 +99,7 @@ try:
     num_col = st.slider("What is the range of columns you want to see?", value=(1, 14),
                         help="Columns = 14")
 
-    disp_col = col_pl[num_col[0]:num_col[1]]
+    disp_col = pl_col[num_col[0]:num_col[1]]
 
     st.write(data[disp_col][num_play[0]:num_play[1]])
 
